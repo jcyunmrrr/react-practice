268713f54dcfc8a9a315c29c0245c1a92f4bf05e
jcyunmrrr <jsmn303@gmail.com>
2024-03-14 18:03:04 +0800

    finish lesson 11

--- codes/01_indecision-app/public/scripts/app.js
+++ codes/01_indecision-app/public/scripts/app.js
@@ -5,7 +5,29 @@ console.log("app.js is running");
 // JSX - JavaScript XML
 
 var appRoot = document.getElementById("app");
-var template = /*#__PURE__*/React.createElement("h1", {
-  id: "someid"
-}, "this is jsx from app.js");
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer"
+};
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "you come first"), /*#__PURE__*/React.createElement("li", null, "wait a second")));
+
+// var userName = "jcyunmrrr";
+// var userAge = 3.14159;
+// var userLocation = "somewhere out there beneath the pale moonlight";
+
+// var templateTwo = (
+//     <div>
+//         <h1>{userName}</h1>
+//         <p>Age: {userAge}</p>
+//         <p>Location: {userLocation}</p>
+//     </div>
+// );
+
+var user = {
+  name: "jcyunmrrr-2",
+  age: 1.414,
+  location: "country road take me home"
+};
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name), /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), /*#__PURE__*/React.createElement("p", null, "Location: ", user.location));
 ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
--- codes/01_indecision-app/src/app.js
+++ codes/01_indecision-app/src/app.js
@@ -3,6 +3,48 @@ console.log("app.js is running")
 // JSX - JavaScript XML
 
 var appRoot = document.getElementById("app");
-var template = <h1 id="someid">this is jsx from app.js</h1>;
+
+var app = {
+    title: "Indecision App", 
+    subtitle: "Put your life in the hands of a computer"
+};
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>you come first</li>
+            <li>wait a second</li>
+        </ol>
+    </div>
+);
+
+// var userName = "jcyunmrrr";
+// var userAge = 3.14159;
+// var userLocation = "somewhere out there beneath the pale moonlight";
+
+// var templateTwo = (
+//     <div>
+//         <h1>{userName}</h1>
+//         <p>Age: {userAge}</p>
+//         <p>Location: {userLocation}</p>
+//     </div>
+// );
+
+var user = {
+    name: "jcyunmrrr-2", 
+    age: 1.414, 
+    location: "country road take me home"
+};
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
 
 ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
