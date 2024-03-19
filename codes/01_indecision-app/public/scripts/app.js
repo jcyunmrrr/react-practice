d3096af17612e37fce056e7084d905baa8937e51
jcyunmrrr <jsmn303@gmail.com>
2024-03-19 14:51:17 +0800

    finish lesson 13

--- codes/01_indecision-app/public/scripts/app.js
+++ codes/01_indecision-app/public/scripts/app.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 console.log("app.js is running");
 
 // JSX - JavaScript XML
@@ -7,29 +11,15 @@ console.log("app.js is running");
 var appRoot = document.getElementById("app");
 var app = {
   title: "Indecision App",
-  subtitle: "Put your life in the hands of a computer"
-  // options: ["a", "bb"], 
+  subtitle: "Put your life in the hands of a computer",
+  options: ["a", "bb"]
 };
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options && app.options.length > 0 ? "Here are your options" : "No option"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "you come first"), /*#__PURE__*/React.createElement("li", null, "wait a second")));
-
-// var userName = "jcyunmrrr";
-// var userAge = 3.14159;
-// var userLocation = "somewhere out there beneath the pale moonlight";
-
-// var templateTwo = (
-//     <div>
-//         <h1>{userName}</h1>
-//         <p>Age: {userAge}</p>
-//         <p>Location: {userLocation}</p>
-//     </div>
-// );
-
-var user = {
-  name: "jcyunmrrr-2",
-  // age: 1.414, 
+var user = _defineProperty({
+  name: "jcyunmrrr",
+  age: 1.414,
   location: "country road take me home"
-  // location: "", 
-};
+}, "location", "");
 function getLocation(location) {
   if (location) {
     return /*#__PURE__*/React.createElement("p", null, "Location: ", location);
--- codes/01_indecision-app/src/app.js
+++ codes/01_indecision-app/src/app.js
@@ -2,15 +2,15 @@ console.log("app.js is running")
 
 // JSX - JavaScript XML
 
-var appRoot = document.getElementById("app");
+const appRoot = document.getElementById("app");
 
-var app = {
+const app = {
     title: "Indecision App", 
     subtitle: "Put your life in the hands of a computer", 
-    // options: ["a", "bb"], 
+    options: ["a", "bb"], 
 };
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -22,23 +22,11 @@ var template = (
     </div>
 );
 
-// var userName = "jcyunmrrr";
-// var userAge = 3.14159;
-// var userLocation = "somewhere out there beneath the pale moonlight";
-
-// var templateTwo = (
-//     <div>
-//         <h1>{userName}</h1>
-//         <p>Age: {userAge}</p>
-//         <p>Location: {userLocation}</p>
-//     </div>
-// );
-
-var user = {
-    name: "jcyunmrrr-2", 
-    // age: 1.414, 
+const user = {
+    name: "jcyunmrrr", 
+    age: 1.414, 
     location: "country road take me home", 
-    // location: "", 
+    location: "", 
 };
 
 function getLocation(location) {
@@ -47,7 +35,7 @@ function getLocation(location) {
     }
 }
 
-var templateTwo = (
+const templateTwo = (
     <div>
         <h1>{user.name ? user.name : "Anonymous"}</h1>
         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
